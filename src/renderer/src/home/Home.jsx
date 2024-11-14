270d8a15d9234b5bf346a8c2671224e3a131d511
Alexis Romero Mendoza <al162917@alumnos.uacj.mx>
2024-11-14 14:16:54 -0700

    adding animation with framer-motion

--- src/renderer/src/home/Home.jsx
+++ src/renderer/src/home/Home.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
-import { FancyButton, HomeContainer, ModuleButton, ModuleContainer, Subtitle, Title } from './Home.styles'
+import { motion } from 'framer-motion' // Importamos motion de framer-motion
+import {
+  FancyButton,
+  HomeContainer,
+  ModuleButton,
+  ModuleContainer,
+  Subtitle,
+  Title
+} from './Home.styles'
 
 export const Home = () => {
   return (
-    <HomeContainer>
-      <Title>¡Bienvenido al ERP SPAyT!</Title>
-      <Subtitle>
-        "El éxito no es la clave de la felicidad; la felicidad es la clave del éxito. Si amas lo que
-        haces, tendrás éxito." ¡Recuerda que cada paso que das te acerca más a tus sueños! ✨
-      </Subtitle>
-      <FancyButton>¿Con cuál modulo vas a trabajar?</FancyButton>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: 'easeOut' }} // Configuración de la animación
+    >
+      <HomeContainer>
+        <Title>¡Bienvenido al ERP SPAyT!</Title>
+        <Subtitle>
+          "El éxito no es la clave de la felicidad; la felicidad es la clave del éxito. Si amas lo
+          que haces, tendrás éxito." ¡Recuerda que cada paso que das te acerca más a tus sueños! ✨
+        </Subtitle>
+        <FancyButton>¿Con cuál modulo vas a trabajar?</FancyButton>
 
-      {/* Nuevo contenedor de botones de módulos */}
-      <ModuleContainer>
-        <ModuleButton to = '/plantillas' >Plantillas</ModuleButton>
-        <ModuleButton to = '/constancias' >Contancias</ModuleButton>
-        <ModuleButton to = '/certificacion' >Certificacion</ModuleButton>
-        <ModuleButton to = '/' >Otros</ModuleButton>
-      </ModuleContainer>
-    </HomeContainer>
+        {/* Nuevo contenedor de botones de módulos */}
+        <ModuleContainer>
+          <ModuleButton to="/plantillas">Plantillas</ModuleButton>
+          <ModuleButton to="/constancias">Contancias</ModuleButton>
+          <ModuleButton to="/certificacion">Certificacion</ModuleButton>
+          <ModuleButton to="/">Otros</ModuleButton>
+        </ModuleContainer>
+      </HomeContainer>
+    </motion.div>
   )
 }
 
